6f6f3dbdebdfc163d47b3e3b983634de762b082f
Benz Barquilla <2201105801@student.buksu.edu.ph>
2024-10-29 19:22:42 +0800

    updated screen sizes

--- GEMBS/client/src/components/LoginPage.jsx
+++ GEMBS/client/src/components/LoginPage.jsx
@@ -9,13 +9,14 @@ function LoginPage() {
   return (
     <div className="container">
       <div className="row">
+
         {/* LOGO CONTAINER */}
-        <div className="col-lg-5 col-md-4 col-sm-12" id="logoContainer">
+        <div className="col-lg-5 col-md-5 col-sm-12" id="logoContainer">
           <img id="logo" src={buksuLogo} alt="" />
         </div>
 
         {/* LOGIN AND TEXT CONTAINER */}
-        <div className="col-lg-7 col-md-4 col-sm-12" id="titleContainer">
+        <div className="col-lg-7 col-md-7 col-sm-12" id="titleContainer">
           <h1>GEMBS</h1>
           <p>
             Find the equipment you need right here <br /> Borrow wisely, return
